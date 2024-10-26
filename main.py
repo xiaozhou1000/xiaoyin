82bd2e6ebc27fc6760f7175d39b5b464171ca56f
xiaozhou1000 <108339718+xiaozhou1000@users.noreply.github.com>
2024-10-26 20:38:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,52 @@
-from datetime import date, datetime
-import math
-from wechatpy import WeChatClient
-from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
-import os
-import random
+import json
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-today = datetime.now()
-start_date = os.environ['START_DATE']
-city = os.environ['CITY']
-birthday = os.environ['BIRTHDAY']
+def get_weather_info():
+    # 这里编写获取天气信息的代码，比如调用天气 API
+    # 返回包含天气相关信息的字典，例如 {'weather': '晴', 'temp': '25℃'}
+    return weather_data
 
-app_id = os.environ["APP_ID"]
-app_secret = os.environ["APP_SECRET"]
+def send_wechat_message(weather_info, appid, secret, template_id, openid):
+    access_token = get_access_token(appid, secret)
+    data = {
+        "touser": openid,
+        "template_id": template_id,
+        "data": {
+            # 根据你的模板内容，填充相应的参数值
+            "date": {"value": "今天的日期"},
+            "region": {"value": "所在地区"},
+            "weather": {"value": weather_info['weather']},
+            "temp": {"value": weather_info['temp']}
+        }
+    }
+    url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=" + access_token
+    response = requests.post(url, json=data)
+    if response.status_code == 200:
+        print("消息推送成功")
+    else:
+        print("消息推送失败：", response.text)
 
-user_id = os.environ["USER_ID"]
-template_id = os.environ["TEMPLATE_ID"]
+def get_access_token(appid, secret):
+    token_url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + appid + "&secret=" + secret
+    response = requests.get(token_url)
+    if response.status_code == 200:
+        result = response.json()
+        if "access_token" in result:
+            return result["access_token"]
+    return None
 
+def main():
+    # 你的微信测试号的 appid、secret、模板 ID 和接收消息的用户 OpenID
+    appid = "wx30afd0b702ac0971"
+    secret = "541b2c5093cc822ec0e5746f5792002b"
+    template_id = "ibHTxtRJdrPDSTmdTk7NWPvFo84W5SLHrsQbqpI2L8k"
+    openid = "o3m8S6wC_BzGi2xQj72FuFKFegAg"
+    weather_info = get_weather_info()
+    send_wechat_message(weather_info, appid, secret, template_id, openid)
 
-def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
-
-def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
-
-def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
-
-def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
-
-def get_random_color():
-  return "#%06x" % random.randint(0, 0xFFFFFF)
-
-
-client = WeChatClient(app_id, app_secret)
-
-wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(user_id, template_id, data)
-print(res)
+if __name__ == "__main__":
+    scheduler = BlockingScheduler()
+    # 每天早上 8 点执行一次消息推送
+    scheduler.add_job(main, 'cron', hour=8)
+    scheduler.start()
