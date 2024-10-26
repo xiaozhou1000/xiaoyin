5fb9f9cd8721fb3240b777ce17bb7f47335d0ba0
xiaozhou1000 <108339718+xiaozhou1000@users.noreply.github.com>
2024-10-26 20:47:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -48,5 +48,5 @@ def main():
 if __name__ == "__main__":
     scheduler = BlockingScheduler()
     # 每天早上 8 点执行一次消息推送
-  scheduler.add_job(main, 'cron', hour=20, minute=44) #晚上8点50推送
+  scheduler.add_job(main, 'cron', hour=20, minute=48) #晚上8点50推送
     scheduler.start()
